b5d8035ffa4405c93c434d331357346d0efef3e7
irfan <irfanfy@code.edu.az.com>
2023-09-13 15:38:38 +0400

    six

--- BOOking.MVC/Controllers/GatewayController.cs
+++ BOOking.MVC/Controllers/GatewayController.cs
@@ -7,14 +7,18 @@ namespace BOOking.MVC.Controllers
     public class GatewayController : Controller
     {
         public readonly AppDbContext _dbContext;
+        private readonly int _gatewayCount;
 
         public GatewayController(AppDbContext appDbContext)
         {
             _dbContext = appDbContext;
+            _gatewayCount = _dbContext.Gateways.Count();
         }
         public IActionResult Index()
         {
-            var gateway = _dbContext.Gateways.ToList();
+            ViewBag.GatewayCount = _gatewayCount;
+
+            var gateway = _dbContext.Gateways.Take(6).ToList();
             
 
             var model = new GatewayViewModel
@@ -24,5 +28,15 @@ namespace BOOking.MVC.Controllers
 
             return View(model);
         }
+
+        public IActionResult LoadGateways(int skip)
+        {
+            if (skip >= _gatewayCount) return BadRequest();
+            var gateways = _dbContext.Gateways.Skip(skip).Take(3).ToList();
+
+            return View("Index", gateways);
+        }
+
+        
     }
 }
--- BOOking.MVC/wwwroot/js/loadmore.js
+++ BOOking.MVC/wwwroot/js/loadmore.js
@@ -28,7 +28,7 @@ $(document).on('click', '#loadMore', function () {
         Load More Hotel
 --------------------------------------*/
 var skip = 4;
-$(document).on('click', '#loadMore', function () {
+$(document).on('click', '#loadHotel', function () {
     var hotelCount = $("#hotelCount").val();
     $.ajax({
         url: "/stays/loadHotels?skip=" + skip,
@@ -38,7 +38,7 @@ $(document).on('click', '#loadMore', function () {
             skip += 4;
 
             if (skip >= hotelCount)
-                $("#loadMore").remove();
+                $("#loadHotel").remove();
 
             console.log(response);
         },
@@ -47,5 +47,31 @@ $(document).on('click', '#loadMore', function () {
         }
     });
 })
+
+    /*------------------------------------
+            Load More Gateway
+    --------------------------------------*/
+    var skip = 4;
+    $(document).on('click', '#loadGatesay', function () {
+        var gatewayCount = $("#gatewayCount").val();
+        $.ajax({
+            url: "/gateway/loadGateways?skip=" + skip,
+            type: "GET",
+            success: function (response) {
+                $("#gatewayRow").append(response);
+                skip += 4;
+
+                if (skip >= gatewayCount)
+                    $("#loadGateway").remove();
+
+                console.log(response);
+            },
+            error: function (xhr) {
+
+            }
+        });
+    })
+
+ 
 
 })(jQuery);	
